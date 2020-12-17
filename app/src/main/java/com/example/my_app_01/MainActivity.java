5c268cdbb393538d37b639c9215ca10f8fd6bd47
Aayush Gulia <aayushgulia07@outlook.com>
2020-12-17 17:01:53 +0530

    edit text name & Submit button added on the main page
    btnSubmit & etName

--- app/src/main/java/com/example/my_app_01/MainActivity.java
+++ app/src/main/java/com/example/my_app_01/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.my_app_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Enter your name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String name = etName.getText().toString().trim();
+
+                }
+            }
+        });
     }
 }
